902a3d1f8ed7c9362ca2c5397f5c24d5941e65f2
Saakkshi <this.sakshi07@gmail.com>
2023-11-03 18:31:29 +0530

    first commit to this repositry

---
+++ es.js
@@ -0,0 +1,11 @@
+// "use strict"
+//  console = 20;
+// console.log(console);
+
+//  x = 20;
+// y = "40";
+//  console.log('x'-'y');
+ document.getElementById(heading);
+
+
+ 
---
+++ operators.js
@@ -0,0 +1,75 @@
+
+// var a = 7;
+// console.log(a--);
+// // console.log(a)
+// console.log(a++);
+// console.log(a++);
+// console.log(a);
+// console.log(a--);
+// console.log(a--);
+// console.log(a++);
+// console.log(++a);
+// console.log(a);
+// let a =10;
+// let b = 12;
+// console.log(a>b)
+// a=15;
+// b=16;
+// c=17;
+// d=15;
+// console.log(a==b&&b==c&&c==a&&a==d)
+// a=10
+// b=15
+// c=a
+// a=b
+// b=c
+// console.log(a)
+// console.log(b)
+//  a=10
+//  b=15
+// a =a+b
+// b=a-b
+//  a=a-b
+//  console.log(a)
+//  console.log(b)
+// age = 18
+// age>18?console.log("true"):console.log("false")
+
+// a='hello'
+// b="hellooo"
+//STRING OPERATOR
+// console.log(a+b)
+a="bhaskar"
+ b="yogi"
+myName =a+b
+
+// console.log("I AM " +Name);
+//console.log(`i am ${myName}`) //template litteral
+
+// a="jai"
+// b="vijay"
+// console.log(`${a} or ${b} `)
+
+// a = "jai"
+// b= "vijay"
+// console.log(a+"*"+b)
+// console.log(`${a} ${b}`)
+// console.log(`${a} * ${b}`)
+// var a = null
+// console.log(typeof(a)) //null is object type datatype
+var x
+// a=5*x
+console.log(typeof(x))
+// j_ = "jai"
+// console.log(j@)
+a="15"+2
+console.log(!isNaN(a))
+
+//arithmetic operator
+//assignment operaator
+//comparison operator
+//exponential operator
+console.log(2**3**2)
+//conditional operator
+// 1.ternary
+
